1854a0c10b7fede604bb9624068485dd89cfc892
Austin Isinghood <austin@adaptixdesign.com>
2018-01-16 22:10:48 -0500

    Adding About content.

--- front-page.php
+++ front-page.php
@@ -5,8 +5,20 @@
 <main class="content"><!-- Content starts here. -->
 
 <header id="intro" class="col-xs-12">
+
   <h1>Placement Professionals</h1>
   <p>Advanced Dental Associates Inc. is a dental staffing referral service serving Chapel Hill, Raleigh, Durham, and surrounding areas of North Carolina. We provide temporary and permanent placements for all dental personnel.</p>
+  <br>
+  <p>Some of our notable services include:
+   <ul class="list-unstyled">
+     <li><span aria-hidden="true" class="glyphicon glyphicon glyphicon-ok"></span> Pre-screened quality candidates</li>
+     <li><span aria-hidden="true" class="glyphicon glyphicon glyphicon-ok"></span> Reference and performance evaluations on file</li>
+     <li><span aria-hidden="true" class="glyphicon glyphicon glyphicon-ok"></span> Licenses and certifications on file</li>
+     <li><span aria-hidden="true" class="glyphicon glyphicon glyphicon-ok"></span> No fee to list or interview</li>
+     <li><span aria-hidden="true" class="glyphicon glyphicon glyphicon-ok"></span> Confirmed interviews and placements</li>
+     <li><span aria-hidden="true" class="glyphicon glyphicon glyphicon-ok"></span> Face to Face Interviews with each candidate</li>
+   </ul>
+ </p>
 </header>
 
 <section id="positions" class="advdental-container col-xs-12">
@@ -74,7 +86,48 @@
 </section>
 
 <section id="about" class="col-xs-12">
+  <div id="about-nav" class="col-xs-12">
+    <ul class="list-unstyled">
+      <li>About Us</li>
+      <li>About Mindy</li>
+    </ul>
+  </div>
+  <div id="about-advdental" class="col-xs-12">
+    <h2>Advanced Dental Associates, Inc. Vision</h2>
+    <div class="col-xs-12 col-sm-6">
+      <h3>Quality Over Quantity</h3>
+      <p>Many of our new clients ask me, why are you different? Many of our regular clients will tell you it’s the commitment to matching the right candidate to the right practice. We believe in the uniqueness of every office and candidate. This is why we value our “face to face” interviews with each dental professional working with us. We feel this is integral to our success of providing quality dental staffing. We realize, not only does the candidate represents us, more importantly they represent our clients to their customers.</p>
 
+      <p>Whether you’re a practice looking to hire their next employee or a dental professional looking for their next permanent position, we have always recommend working interviews. Many years ago this was not the practice. Today, it’s key to ensuring the proper fit and harmony in any practice. Our model that has been developed with decades of success is for the potential new hire to work in the office for a day or two. After the choice is made, we guarantee that permanent placement with a 30/60 day limited guarantee.</p>
+
+      <p>We are proud to serve the dental community with awesome staff through constant recruiting and the building of relationships. If you’re in the dental community today and are ready to make a change, please feel free to reach out to Advance Dental Associates Inc. by emailing mindy@advdentaltemps.com. We’re looking forward to serving you.</p>
+    </div>
+    <div class="col-xs-12 col-sm-6">
+      <h3>We Serve The Entire Team!</h3>
+      <p>Job satisfaction is a major part of personal happiness. Career opportunities of a permanent and temporary nature are available through Advanced Dental Associates. If you seek job relocation, a new beginning or are returning to the work force, we can meet your every employment need.</p>
+      <p>
+        <ul class="list-unstyled">
+          <li>Commitment to recruitment</li>
+          <li>Licenses & certifications on file</li>
+          <li>24 hour emergency service</li>
+          <li>Confirmed placements</li>
+          <li>No fee to list or interview</li>
+          <li>Working interviews</li>
+        </ul>
+      </p>
+    </div>
+  </div>
+  <div id="about-mindy" class="col-xs-12">
+    <h3>About Mindy</h3>
+    <img src="<?php echo get_template_directory_uri(); ?>/img/placeholder-logo.png" alt="Advanced Dental Associates, Inc. logo.">
+    <p>Mindy Lazear Gill is a graduate from West Liberty University with a BA degree. My career in the dental field started with Hygiene Associates Inc. in Rockville, Maryland, under the direction of Pamela Lazear Quinones, RDH. What began as a 6-month temporary job quickly turned into a lifelong passion with dental staffing. Together we made a great team, Pam’s passion for dental hygiene and staffing built a successful business helping dental offices in the Metro area to realizing their full potential.</p>
+
+    <p>After 11 years in the Washington DC Metro area, my husband took a transfer to Raleigh, NC. During the initial years I spent time getting my children settled into school and volunteering where needed in many school functions. Though I was very active in my children’s early development, I never lost interest in the dental community. After several years of careful planning, it was time to bring the Hygiene Associates’ model to the Raleigh area by opening the doors to Advance Dental Associates Inc.</p>
+
+    <p>Today, I am active in the NC Dental Community. I’m a current member of the Durham-Orange Hygiene Association. ADA Inc. supports, exhibits and sponsors at the meetings of the NC Dental Society, NCAGD, NCDHA, NCMOMs, TADS, The Cleft Palate Gallop 5K and the AADOM Triangle Chapter.</p>
+
+    <p>In summary, communities are what we make of them. The active involvement and volunteering makes NC Dental Communities one of the best in the nation.</p>
+  </div>
 </section>
 
 <section id="info" class="col-xs-12">

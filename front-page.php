7dd496ffeff540caa604511da824030854b7641b
Austin Isinghood <austin@adaptixdesign.com>
2018-01-18 20:52:31 -0500

    Adding photos to positions section.

--- front-page.php
+++ front-page.php
@@ -10,7 +10,7 @@
   <h1>Placement Professionals</h1>
   <p>Advanced Dental Associates Inc. is a dental staffing referral service serving Chapel Hill, Raleigh, Durham, and surrounding areas of North Carolina. We provide temporary and permanent placements for all dental personnel.</p>
   <div class="down-arrow">
-    <a href="#positions"><span aria-hidden="true" class="blinkme glyphicon glyphicon-chevron-down"></span></a>
+    <a href="#intro-list"><span aria-hidden="true" class="blinkme glyphicon glyphicon-chevron-down"></span></a>
   </div>
 </header>
 <!-- END CONTACT SECTION -->
@@ -34,69 +34,74 @@
 <!-- BEGIN POSITIONS SECTION -->
 <section id="positions" class="col-xs-12">
 
-  <div class="advdental-container">
+  <div class="row">
 
-    <!-- Dentist section begins here. -->
-    <div class="position col-xs-12">
-      <div class="visible-xs col-xs-12 col-sm-6">
-        <img class="visible-xs" src="<?php echo get_template_directory_uri(); ?>/img/placeholder-logo.png" alt="Advanced Dental Associates, Inc. logo.">
-      </div>
-      <div id="dentist" class="col-xs-12 col-sm-6">
-        <!-- Image loaded in CSS. -->
-      </div>
-      <div class="position__content col-xs-12 col-sm-6">
-        <h2>Dentists</h2>
-        <p>Our dentist are all licensed in the state of North Carolina and carry all current credentials, which are on file and updated in our office, yearly.</p>
-      </div>
-    </div>
-    <!-- Dentist section ends here. -->
+    <div class="advdental-container">
 
-    <!-- Dental Hygienists section begins here. -->
-    <div class="position col-xs-12">
-      <div class="visible-xs col-xs-12 col-sm-6">
-        <img src="<?php echo get_template_directory_uri(); ?>/img/placeholder-logo.png" alt="Advanced Dental Associates, Inc. logo.">
+      <!-- Dentist section begins here. -->
+      <div class="position col-xs-12">
+        <div class="visible-xs col-xs-12 col-sm-6">
+          <img class="visible-xs" src="<?php echo get_template_directory_uri(); ?>/img/dentist.jpg" alt="Photo of Dentist.">
+        </div>
+        <div id="dentist" class="col-xs-12 col-sm-6">
+          <!-- Image loaded in CSS. -->
+        </div>
+        <div class="position__content col-xs-12 col-sm-6">
+          <h2>Dentists</h2>
+          <p>Our dentist are all licensed in the state of North Carolina and carry all current credentials, which are on file and updated in our office, yearly.</p>
+        </div>
       </div>
-      <div class="position__content col-xs-12 col-sm-6">
-        <h2>Dental Hygienists</h2>
-        <p>Dental hygienists either have an associate's or bachelor's degree from an accredited college in order to work as a licensed professional.</p>
-      </div>
-      <div id="dental-hygienist" class="col-xs-12 col-sm-6">
-        <!-- Image loaded in CSS. -->
-      </div>
-    </div>
-    <!-- Dental Hygienists section ends here. -->
+      <!-- Dentist section ends here. -->
 
-    <!-- Dental Assistants section begins here. -->
-    <div class="position col-xs-12">
-      <div class="visible-xs col-xs-12 col-sm-6">
-        <img src="<?php echo get_template_directory_uri(); ?>/img/placeholder-logo.png" alt="Advanced Dental Associates, Inc. logo.">
+      <!-- Dental Hygienists section begins here. -->
+      <div class="position col-xs-12">
+        <div class="visible-xs col-xs-12 col-sm-6">
+          <img src="<?php echo get_template_directory_uri(); ?>/img/dental-hygienist.jpg" alt="Photo of Dental Hygienist.">
+        </div>
+        <div class="position__content col-xs-12 col-sm-6">
+          <h2>Dental Hygienists</h2>
+          <p>Dental hygienists either have an associate's or bachelor's degree from an accredited college in order to work as a licensed professional.</p>
+        </div>
+        <div id="dental-hygienist" class="col-xs-12 col-sm-6">
+          <!-- Image loaded in CSS. -->
+        </div>
       </div>
-      <div id="dental-assistant" class="col-xs-12 col-sm-6">
-        <!-- Image loaded in CSS. -->
-      </div>
-      <div class="position__content col-xs-12 col-sm-6">
-        <h2>Dental Assistants</h2>
-        <p>We recruit and place temporary and permanent dental assistants that are upbeat, dependable, reliable and qualified.</p>
-      </div>
-    </div>
-    <!-- Dental Assistants section ends here. -->
+      <!-- Dental Hygienists section ends here. -->
 
-    <!-- Front Office Staff section begins here. -->
-    <div class="position col-xs-12">
-      <div class="visible-xs col-xs-12 col-sm-6">
-        <img src="<?php echo get_template_directory_uri(); ?>/img/placeholder-logo.png" alt="Advanced Dental Associates, Inc. logo.">
+      <!-- Dental Assistants section begins here. -->
+      <div class="position col-xs-12">
+        <div class="visible-xs col-xs-12 col-sm-6">
+          <img src="<?php echo get_template_directory_uri(); ?>/img/dental-assistant.jpg" alt="Photo of Dental Assistant.">
+        </div>
+        <div id="dental-assistant" class="col-xs-12 col-sm-6">
+          <!-- Image loaded in CSS. -->
+        </div>
+        <div class="position__content col-xs-12 col-sm-6">
+          <h2>Dental Assistants</h2>
+          <p>We recruit and place temporary and permanent dental assistants that are upbeat, dependable, reliable and qualified.</p>
+        </div>
       </div>
-      <div class="position__content col-xs-12 col-sm-6">
-        <h2>Front Office Staff</h2>
-        <p>Good front office staff is a valuable resource in a dentist office. We are always looking for qualified office managers to work with.</p>
-      </div>
-      <div id="front-office-staff" class="col-xs-12 col-sm-6">
-        <!-- Image loaded in CSS. -->
-      </div>
-    </div>
-    <!-- Front Office Staff section ends here. -->
+      <!-- Dental Assistants section ends here. -->
 
-  </div>
+      <!-- Front Office Staff section begins here. -->
+      <div class="position col-xs-12">
+        <div class="visible-xs col-xs-12 col-sm-6">
+          <img src="<?php echo get_template_directory_uri(); ?>/img/office-manager.jpg" alt="Photo of front office staff personel.">
+        </div>
+        <div class="position__content col-xs-12 col-sm-6">
+          <h2>Front Office Staff</h2>
+          <p>Good front office staff is a valuable resource in a dentist office. We are always looking for qualified office managers to work with.</p>
+        </div>
+        <div id="front-office-staff" class="col-xs-12 col-sm-6">
+          <!-- Image loaded in CSS. -->
+        </div>
+      </div>
+      <!-- Front Office Staff section ends here. -->
+
+    </div><!-- Ends advdental-container. -->
+
+  </div><!-- Ends row. -->
+
 </section>
 <!-- END POSITIONS SECTION -->
 

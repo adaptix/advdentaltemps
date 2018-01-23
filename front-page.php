cadfb5252e2c53f0bf116dc17bb2fafd52ca3b6e
Austin Isinghood <austin@adaptixdesign.com>
2018-01-22 22:49:57 -0500

    More styling to about, info, and contact sections.

--- front-page.php
+++ front-page.php
@@ -27,6 +27,7 @@
        <li><span aria-hidden="true" class="glyphicon glyphicon glyphicon-ok"></span> Confirmed interviews and placements</li>
        <li><span aria-hidden="true" class="glyphicon glyphicon glyphicon-ok"></span> Face to Face Interviews with each candidate</li>
      </ul>
+     <a class="btn btn-default" href="#positions">View our positions</a>
   </div>
 </div>
 <!-- END INTRO LIST -->
@@ -120,24 +121,24 @@
 
     <!-- Begin about Advanced Dental Associates, Inc. section. -->
     <div id="about-advdental" class="col-xs-12">
-      <div class="col-xs-12 col-sm-6">
-        <h3>Quality Over Quantity</h3>
+      <div class="col-xs-12 col-md-7">
+        <h2>Quality Over Quantity</h2>
         <p>Many of our new clients ask me, why are you different? Many of our regular clients will tell you it’s the commitment to matching the right candidate to the right practice. We believe in the uniqueness of every office and candidate. This is why we value our “face to face” interviews with each dental professional working with us. We feel this is integral to our success of providing quality dental staffing. We realize, not only does the candidate represents us, more importantly they represent our clients to their customers.</p>
 
         <p>Whether you’re a practice looking to hire their next employee or a dental professional looking for their next permanent position, we have always recommend working interviews. Many years ago this was not the practice. Today, it’s key to ensuring the proper fit and harmony in any practice. Our model that has been developed with decades of success is for the potential new hire to work in the office for a day or two. After the choice is made, we guarantee that permanent placement with a 30/60 day limited guarantee.</p>
 
         <p>We are proud to serve the dental community with awesome staff through constant recruiting and the building of relationships. If you’re in the dental community today and are ready to make a change, please feel free to reach out to Advance Dental Associates Inc. by emailing mindy@advdentaltemps.com. We’re looking forward to serving you.</p>
       </div>
-      <div class="col-xs-12 col-sm-6">
-        <h3>We Serve The Entire Team!</h3>
+      <div class="col-xs-12 col-md-5">
+        <h2>We Serve The Entire Team!</h2>
         <p>Job satisfaction is a major part of personal happiness. Career opportunities of a permanent and temporary nature are available through Advanced Dental Associates. If you seek job relocation, a new beginning or are returning to the work force, we can meet your every employment need.</p>
         <ul class="list-unstyled">
-          <li>Commitment to recruitment</li>
-          <li>Licenses & certifications on file</li>
-          <li>24 hour emergency service</li>
-          <li>Confirmed placements</li>
-          <li>No fee to list or interview</li>
-          <li>Working interviews</li>
+          <li><span aria-hidden="true" class="glyphicon glyphicon glyphicon-ok"></span> Commitment to recruitment</li>
+          <li><span aria-hidden="true" class="glyphicon glyphicon glyphicon-ok"></span> Licenses & certifications on file</li>
+          <li><span aria-hidden="true" class="glyphicon glyphicon glyphicon-ok"></span> 24 hour emergency service</li>
+          <li><span aria-hidden="true" class="glyphicon glyphicon glyphicon-ok"></span> Confirmed placements</li>
+          <li><span aria-hidden="true" class="glyphicon glyphicon glyphicon-ok"></span> No fee to list or interview</li>
+          <li><span aria-hidden="true" class="glyphicon glyphicon glyphicon-ok"></span> Working interviews</li>
         </ul>
       </div>
     </div>
@@ -145,8 +146,8 @@
 
     <!-- Begin about Mindy section. -->
     <div id="about-mindy" class="col-xs-12">
-      <h3>About Mindy</h3>
-      <img src="<?php echo get_template_directory_uri(); ?>/img/placeholder-logo.png" alt="Advanced Dental Associates, Inc. logo.">
+      <h2>About Mindy</h2>
+      <img src="<?php echo get_template_directory_uri(); ?>/img/mindy.png" alt="Photo of Mindy Gill.">
       <p>Mindy Lazear Gill is a graduate from West Liberty University with a BA degree. My career in the dental field started with Hygiene Associates Inc. in Rockville, Maryland, under the direction of Pamela Lazear Quinones, RDH. What began as a 6-month temporary job quickly turned into a lifelong passion with dental staffing. Together we made a great team, Pam’s passion for dental hygiene and staffing built a successful business helping dental offices in the Metro area to realizing their full potential.</p>
 
       <p>After 11 years in the Washington DC Metro area, my husband took a transfer to Raleigh, NC. During the initial years I spent time getting my children settled into school and volunteering where needed in many school functions. Though I was very active in my children’s early development, I never lost interest in the dental community. After several years of careful planning, it was time to bring the Hygiene Associates’ model to the Raleigh area by opening the doors to Advance Dental Associates Inc.</p>
@@ -165,37 +166,37 @@
 <section id="info" class="col-xs-12">
   <div class="advdental-container">
     <div id="resources" class="col-xs-12 col-sm-9">
-      <div class="resource col-xs-2 col-sm-4">
+      <div class="resource col-xs-4">
         <img src="<?php echo get_template_directory_uri(); ?>/img/placeholder-logo.png" alt="Advanced Dental Associates, Inc. logo.">
       </div>
-      <div class="resource col-xs-2 col-sm-4">
+      <div class="resource col-xs-4">
         <img src="<?php echo get_template_directory_uri(); ?>/img/placeholder-logo.png" alt="Advanced Dental Associates, Inc. logo.">
       </div>
-      <div class="resource col-xs-2 col-sm-4">
+      <div class="resource col-xs-4">
         <img src="<?php echo get_template_directory_uri(); ?>/img/placeholder-logo.png" alt="Advanced Dental Associates, Inc. logo.">
       </div>
-      <div class="resource col-xs-2 col-sm-4">
+      <div class="resource col-xs-4">
         <img src="<?php echo get_template_directory_uri(); ?>/img/placeholder-logo.png" alt="Advanced Dental Associates, Inc. logo.">
       </div>
-      <div class="resource col-xs-2 col-sm-4">
+      <div class="resource col-xs-4">
         <img src="<?php echo get_template_directory_uri(); ?>/img/placeholder-logo.png" alt="Advanced Dental Associates, Inc. logo.">
       </div>
-      <div class="resource col-xs-2 col-sm-4">
+      <div class="resource col-xs-4">
         <img src="<?php echo get_template_directory_uri(); ?>/img/placeholder-logo.png" alt="Advanced Dental Associates, Inc. logo.">
       </div>
-      <div class="resource col-xs-2 col-sm-4">
+      <div class="resource col-xs-4">
         <img src="<?php echo get_template_directory_uri(); ?>/img/placeholder-logo.png" alt="Advanced Dental Associates, Inc. logo.">
       </div>
-      <div class="resource col-xs-2 col-sm-4">
+      <div class="resource col-xs-4">
         <img src="<?php echo get_template_directory_uri(); ?>/img/placeholder-logo.png" alt="Advanced Dental Associates, Inc. logo.">
       </div>
-      <div class="resource col-xs-2 col-sm-4">
+      <div class="resource col-xs-4">
         <img src="<?php echo get_template_directory_uri(); ?>/img/placeholder-logo.png" alt="Advanced Dental Associates, Inc. logo.">
       </div>
     </div>
     <div id="referral" class="col-xs-12 col-sm-3">
       <h3>For clients & temps</h3>
-      <a href="">Dental Referral Slip</a>
+      <a class="btn btn-default" href="">Dental Referral Slip</a>
     </div>
   </div>
 </section>
@@ -204,7 +205,6 @@
 <!-- BEGIN CONTACT SECTION -->
 <section id="contact" class="col-xs-12">
   <div class="advdental-container">
-    <h2>Contact Us</h2>
 
       <!-- Begin forms. -->
       <div id="forms" class="col-xs-12 col-sm-6">
@@ -212,8 +212,8 @@
         <!-- Begin forms navigation. -->
         <div id="forms-nav">
           <ul class="list-unstyled">
-            <li id="form1" class="active">Client Form</li> |
-            <li id="form2">Applicant Form</li>
+            <li id="form1" class="active">Client</li> |
+            <li id="form2">Applicant</li>
           </ul>
         </div>
         <!-- End forms navigation. -->
